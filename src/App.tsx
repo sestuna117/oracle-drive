98ff839cfea3c82d81dece5cc07c8f9e227fbbf8
JGuo <jason.guo2560@gmail.com>
2021-09-26 16:46:28 -0400

    v0.3.1 - started work on card table - display basic table information

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import ContextLoader from "./hook";
 import CardsPage from "./components/CardsPage";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
     <ContextLoader>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/cards">Cards</Link>
-              </li>
-              <li>
-                <Link to="/npcs">NPCs</Link>
-              </li>
-              <li>
-                <Link to="/packs">Packs</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/cards">
-              <CardsPage />
-            </Route>
-            <Route path="/npcs">
-              <div>NPCs</div>
-            </Route>
-            <Route path="/packs">
-              <div>Packs</div>
-            </Route>
-            <Route path="/">
-              <div>Home</div>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Router>
+          <div>
+            <NavBar />
+            <Switch>
+              <Route path="/cards">
+                <CardsPage />
+              </Route>
+              <Route path="/npcs">
+                <div>NPCs</div>
+              </Route>
+              <Route path="/packs">
+                <div>Packs</div>
+              </Route>
+              <Route path="/">
+                <div>Home</div>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
     </ContextLoader>
   );
 }
--- src/components/CardsPage.tsx
+++ src/components/CardsPage.tsx
@@ -1,13 +1,44 @@
-import React, {useContext} from 'react';
-import {CardContext} from "../hook";
+import React, { useContext } from "react";
+import { CardContext } from "../hook";
+import "./CardsPage.css";
 
 export default function CardsPage() {
-    const cardsData = useContext(CardContext);
-    console.log(cardsData);
+  const cardsData = useContext(CardContext);
+  console.log(cardsData);
 
-    if (cardsData) {
-        console.log(cardsData.results);
-    }
+  if (cardsData) {
+    console.log(cardsData.results);
+  }
 
-    return null;
+  return cardsData ?
+      <div className="card-list">
+          <table className="card-table">
+              <thead>
+                <tr>
+                    <th>Id</th>
+                    <th className="cell-align-left">Name</th>
+                    <th>Type</th>
+                    <th>Rarity</th>
+                    <th>Stats</th>
+                    <th className="cell-align-left">Source</th>
+                    <th>Owned</th>
+                    <th>Patch</th>
+                </tr>
+              </thead>
+              <tbody>
+              {cardsData.results.map(card => (
+                  <tr className="card-table-row" key={card.id}>
+                      <td><img src={card.icon} alt={card.name +"_icon"}/></td>
+                      <td className="cell-align-left"><span>{card.name}</span></td>
+                      <td>type</td>
+                      <td><span>{card.stars}</span></td>
+                      <td>stats</td>
+                      <td className="cell-align-left">source</td>
+                      <td><span>{card.owned}</span></td>
+                      <td><span>{card.patch}</span></td>
+                  </tr>
+              ))}
+              </tbody>
+          </table>
+      </div> : null;
 }
